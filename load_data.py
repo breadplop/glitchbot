721df9818a49822d365213d7306ca184e90f4dfd
Glitch (truth-mine) <none>
2019-01-14 05:08:35 +0000

    👡☄️ Updated with Glitch 14th Jan

--- load_data.py
+++ load_data.py
@@ -18,12 +18,12 @@ def format_menu(menu):
     res = menu.to_string(index=False, header=False, na_rep="")
 
     words_to_bold = ['Breakfast', 'Dinner']
-    words_to_italicized = ['Choose 1', 'Drinks', 'Meat', 'Side Dish', 'Vegetable', 'Dessert']
+    words_to_italicized = ['Choose 1', 'Drinks', 'Meat', 'Side Dish', '-Soup-', 'Main', 'Vegetable', 'Dessert', 'Set A', 'Set B']
     res.replace(" ","")
     for word in words_to_bold:
-        res =res.replace(word, '*😊' + word.upper() + '😊*') #(' + dtime + ')')
+        res =res.replace(word, '*' + word.upper() + '*') #(' + dtime + ')')
     for word in words_to_italicized:
-        res = res.replace(word, '_-' + word + '_-')
+        res = res.replace(word, '_' + word + '_')
     res = res.replace("/restart","")
     res = res.replace("  ","")
     res = res.replace("\n ","\n")
